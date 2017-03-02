4a6558f19bfdda3465094497820c16ec16131242
dalekellytoo <dale@dalekelly.org>
2017-03-02 18:44:18 -0500

    Add files via upload

---
+++ XYZ_to_xy.m
@@ -0,0 +1,22 @@
+function XYZ_to_xy();
+
+# edited by Dale Kelly 7/7/2015 dale@dalekelly.org
+# XYZ from http://cvrl.ioo.ucl.ac.uk/
+# XYZ to x,y formula at https://en.wikipedia.org/wiki/CIE_1931_color_space
+
+	fileID = fopen('visual_xy_locus.txt','w');
+	nm_and_XYZ=csvread("ciexyz31_1.csv");
+
+	i=1;
+
+	while (i <= 471)
+		nm = nm_and_XYZ(i,1);
+		denom = 1 / (nm_and_XYZ(i,2) + nm_and_XYZ(i,3) + nm_and_XYZ(i,4));
+		x = nm_and_XYZ(i,2) * denom;
+		y = nm_and_XYZ(i,3) * denom;
+		fprintf(fileID,'%i %f %f\r\n',nm, x, y);
+		i=i+1;
+	endwhile
+	fclose(fileID);
+	
+endfunction
